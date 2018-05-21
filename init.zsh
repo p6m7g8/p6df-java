09d1b8aed6d0d30b0479bbf7df5eebcc09d04a9a
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-21 12:28:05 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -12,5 +12,24 @@ p6df::modules::java::external::brew() {
 
 p6df::modules::java::init() {
 
+  p6df::modules::java::jenv::init
 }
+
+p6df::modules::java::jenv::init() {
+    [ -n "$DISABLE_ENVS" ] && return
+
+    export JENV_ROOT=/Users/pgollucci/.local/share/gcuisinier/jenv
+    p6dfz::util::path_if $JENV_ROOT/bin
+
+    if [ -x $JENV_ROOT/bin/jenv ]; then
+      export HAS_JAENV=1
+      eval "$(jenv init - zsh)"
+    fi
+}
+
+p6df::prompt::java::line() {
+
+  env_version "j"
+}
+
 p6df::modules::java::init
